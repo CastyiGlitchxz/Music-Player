7d5940c265b22370107cb5d8ad2c2e89b4261edb
CastyiGlitchxz <90124265+CastyiGlitchxz@users.noreply.github.com>
2023-07-12 23:51:59 +0000

    More Code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import flask
 from flask import Flask
 from flask_socketio import SocketIO
+import pymongo
+
+client = pymongo.MongoClient('mongodb+srv://Main-Server:Musicdbaccess@cluster0.0o1no0a.mongodb.net/?retryWrites=true&w=majority')
+db = client.csd
+
+print (db)
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
@@ -14,5 +21,3 @@ def index():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8080)
-
-# Musicdbaccess
